cdcddc0d851504c7b775f0b75d5c47a5df73ab4e
awo.ali <awoali@hotmail.co.uk>
2022-06-08 12:28:09 +0100

    delete unnecessary info

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
 
-import './App.css';
+import './App.scss';
+import EmployeeCard from './components/EmployeeCard/EmployeeCard'
+import Counter from './components/Counter/Counter'
+const App = () => {
+  return (
+    <div className='app'>
+    <h1>Ticket Tracker</h1>
+    <div>
+    <EmployeeCard nameTag = "Name:" roleTag = "Role:"/>
+    <Counter title = "Counter" buttonText = "-" textButton = "+"/>
+    </div>
+    </div>
+  )
+}
+
+export default App
 
